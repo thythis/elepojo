c1033e3b62ce930709199c549ef73509ffa9795b
Leo <thynisang@gmail.com>
2017-11-06 09:45:59 +0800

    thysama1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-message"></i>
-              <span>导航一</span>
+              <span>thy</span>
             </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
